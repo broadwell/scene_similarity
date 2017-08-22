9de99303a7a2bacbecc36c0da92846aedefc444a
Peter Leonard <pleonard@gmail.com>
2017-08-22 17:08:55 -0400

    Put scenes in their own directory

--- video_split.sh
+++ video_split.sh
@@ -5,7 +5,7 @@ filebasename=${1##*/}
 videobasename=${filebasename%.*}
 
 # Make directories to hold images and metadata
-mkdir -p scenes
+mkdir -p scenes/${videobasename}/
 mkdir -p metadata
 
 # Extract keyframes. Use tilde because both - and _ are present in Google ID's,
@@ -13,15 +13,15 @@ mkdir -p metadata
 # to separate GUID from frame number.
 # Use up to four digits for frame namespace in case # of scenes is high.
 
-ffmpeg -i $1  -vf "select=eq(pict_type\,I)" -vsync vfr -q:v 2 scenes/${videobasename}~%04d.jpg
+ffmpeg -i $1  -vf "select=eq(pict_type\,I)" -vsync vfr -q:v 2 scenes/${videobasename}/${videobasename}~%04d.jpg
 # Get the keyframe numbers
 ffprobe -select_streams v -show_frames -show_entries frame=pict_type -of csv $1 | grep -n I | cut -d ':' -f 1 > metadata/${videobasename}_keyframe_indices.txt
 
 
 # Rename keyframes with their frame numbers
-ls -1 scenes/${videobasename}~*.jpg | xargs -n 1 basename > metadata/${videobasename}_keyframe_thumbnails.txt
+ls -1 scenes/${videobasename}/${videobasename}~*.jpg | xargs -n 1 basename > metadata/${videobasename}_keyframe_thumbnails.txt
 paste metadata/${videobasename}_keyframe_thumbnails.txt metadata/${videobasename}_keyframe_indices.txt > metadata/${videobasename}_keyframes.txt
-while read -r thumbnail index; do newIndex=$(echo $index - 1 | bc); mv -- "scenes/$thumbnail" "scenes/$videobasename~$newIndex.jpg"; done < metadata/${videobasename}_keyframes.txt
+while read -r thumbnail index; do newIndex=$(echo $index - 1 | bc); mv -- "scenes/$videobasename/$thumbnail" "scenes/$videobasename/$videobasename~$newIndex.jpg"; done < metadata/${videobasename}_keyframes.txt
 # 
 # Cleanup
 rm metadata/${videobasename}_keyframe_indices.txt
