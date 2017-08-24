6de3ab8816b4decf8ef1ed37d3f83840bc80e840
Peter Leonard <pleonard@gmail.com>
2017-08-24 13:54:30 -0400

    bringing up to date

--- video_split.sh
+++ video_split.sh
@@ -13,11 +13,11 @@ mkdir -p metadata
 # to separate GUID from frame number.
 # Use up to four digits for frame namespace in case # of scenes is high.
 
-ffmpeg -i $1  -vf "select=eq(pict_type\,I)" -vsync vfr -q:v 2 scenes/${videobasename}/${videobasename}~%04d.jpg
+ffmpeg -i $1  -vf "select=eq(pict_type\,I)" -vsync vfr -q:v 2 scenes/${videobasename}/${videobasename}~%04d.jpg  </dev/null
 # Get the keyframe numbers
 #ffprobe -select_streams v -show_frames -show_entries frame=pict_type -of csv $1 | grep -n I | cut -d ':' -f 1 > metadata/${videobasename}_keyframe_indices.txt
 # Get the timestamp in seconds
-ffprobe -select_streams v -show_frames -of csv $1 | grep -n I | cut -d ',' -f 6 | awk '{print int($1)}' > metadata/${videobasename}_keyframe_timestamps.txt
+ffprobe -select_streams v -show_frames -of csv $1 | grep -n I | cut -d ',' -f 6 | awk '{print int($1)}' > metadata/${videobasename}_keyframe_timestamps.txt  
 
 
 
@@ -27,6 +27,7 @@ paste metadata/${videobasename}_keyframe_thumbnails.txt metadata/${videobasename
 while read -r thumbnail index; do newIndex=$(echo $index - 1 | bc); mv -- "scenes/$videobasename/${thumbnail}" "scenes/$videobasename/${videobasename}~${newIndex}.jpg"; done < metadata/${videobasename}_keyframes.txt
 # Edge case where the first frame gets assigned to negative one seconds. Fix by renaming to zero.
 mv  "scenes/$videobasename/${videobasename}~-1.jpg"  "scenes/$videobasename/${videobasename}~0.jpg"
+
 # 
 # Cleanup
 rm metadata/${videobasename}_keyframe_timestamps.txt
