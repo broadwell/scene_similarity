7a49b295ae43f8351ebbeebd7346e7c2641389e3
Peter Leonard <pleonard@gmail.com>
2017-08-24 14:07:00 -0400

    Add in ffprobe

--- video_split.py
+++ video_split.py
@@ -19,9 +19,18 @@ for video in videos[0:3]:
 	# Get the basename of the video
 	video_basename = os.path.splitext(os.path.basename(video))[0]
 	print "Now analyzing " + video
-	# Create a directory to hold the scene thumbnails
+	# Create a directory to hold the keyframe thumbnails
 	if not os.path.exists('scenes/' + video_basename):
 		os.makedirs('scenes/' + video_basename)
-	ffmpeg_firstcommand =  'ffmpeg -i ' + video + ' -vf "select=eq(pict_type\,I)" -vsync vfr -q:v 2 scenes/' + video_basename + '/' + video_basename + '~%04d.jpg < /dev/null'
-	print ffmpeg_firstcommand
-	subprocess.call(ffmpeg_firstcommand, shell=True)
+	# Construct the ffmpeg command to get keyframes
+	ffmpeg_getiframes =  'ffmpeg -i ' + video + ' -vf "select=eq(pict_type\,I)" -vsync vfr -q:v 2 scenes/' + video_basename + '/' + video_basename + '~%04d.jpg < /dev/null'
+	print 'Now executing: ' + ffmpeg_getiframes
+	# Call ffmpeg
+	subprocess.call(ffmpeg_getiframes, shell=True)
+
+	# Construct the ffprobe command to get the timestamp of each keyframe in seconds
+	ffprobe_gettimestamps = "ffprobe -select_streams v -show_frames -of csv " + video + " | grep -n I | cut -d ',' -f 6 | awk '{print int($1)}' > metadata/" + video_basename + "_keyframe_timestamps.txt"
+	print 'Now executing: ' + ffprobe_gettimestamps
+	# Call ffprobe
+	subprocess.call(ffprobe_gettimestamps, shell=True)
+
